a525f00054690f2b5a6e8a23ab11cddf45bee66a
Rafael Costa <rafael.arthur.costa@protonmail.com>
2026-06-06 18:02:13 -0400

    Started Asteroids game, have screen with player

---
+++ main.py
@@ -0,0 +1,40 @@
+import pygame
+
+from constants import SCREEN_WIDTH
+from constants import SCREEN_HEIGHT
+from constants import PLAYER_RADIUS
+from constants import LINE_WIDTH
+
+from logger import log_state
+import player
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0.0
+    player_1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while (True):
+        log_state()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            pass
+
+        screen.fill("black")
+        player_1.draw(screen)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
+    # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
